887cb557a37dabb171c65d38f5eac8050ceb05f9
leoke599 <leoke073@gmail.com>
2025-10-30 09:52:33 -0600

    finished. added indicator of which hashes couldnt be cracked

--- password_cracker.py
+++ password_cracker.py
@@ -2,6 +2,7 @@ import hashlib
 
 def crack_passwords(hash_file_path, wordlist_path):
     hash_set = set()
+    cracked_hashes = set()
 
     with open(f'{hash_file_path}', 'r') as f:
 
@@ -16,16 +17,18 @@ def crack_passwords(hash_file_path, wordlist_path):
             encoded_word = hashlib.md5(clean_word.encode())
             hashed_word = encoded_word.hexdigest()
             if hashed_word in hash_set:
-                print(f"Cracked: {hashed_word} -> {clean_word}")
-            else:
-                print(f"Failed: {hashed_word}")
-            
-
+                print(f"Cracked: {hashed_word} --> {clean_word}")
+                cracked_hashes.add(hashed_word)
+    
+    uncracked_hashes = hash_set - cracked_hashes
+    if uncracked_hashes:
+        print("\n--- Hashes that could NOT be cracked ---")
+        for hash_value in uncracked_hashes:
+            print(f"Not Cracked: {hash_value}")
 
 def main():
     hash_file_path = input("Enter path to hash file: ")
     wordlist_path = input("Enter path to wordlist file: ")
     crack_passwords(hash_file_path, wordlist_path)
-    print("running main")
 
 main()
